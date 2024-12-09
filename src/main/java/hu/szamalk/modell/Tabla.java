49daa250dcdc977f6e6ead5c6145c56d9c807202
dominikdobos <dominik.dobos33@gmail.com>
2024-12-09 09:19:18 +0100

    f1 3

--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -4,5 +4,14 @@ public class Tabla {
     private char[][] T;
     private char uresCella;
 
-    
+    public Tabla(char karakter) {
+        this.uresCella = karakter;
+        this.T = new char[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.T[i][j] = uresCella;
+            }
+        }
+    }
 }
