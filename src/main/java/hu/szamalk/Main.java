fdbb49a27b8e82b7d2fe690a7a3d7fa44e72fb36
dominikdobos <dominik.dobos33@gmail.com>
2024-12-09 09:50:51 +0100

    f1 6

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -5,10 +5,17 @@ import hu.szamalk.modell.Tabla;
 public class Main {
     public static void main(String[] args) {
         feladat4();
+        feladat6();
     }
 
     public static void feladat4() {
         Tabla t = new Tabla('#');
         System.out.println("4. feladat: Az üres tábla:\n" + t.megjelenit());
+    }
+
+    public static void feladat6() {
+        Tabla t = new Tabla('#');
+        t.elhelyez(10);
+        System.out.println("6. feladat: A feltöltött tábla:\n" + t.megjelenit());
     }
 }
